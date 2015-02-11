3590584409a848a003325e2dfe840fcc854c7921
Gavin Dunn <gavin.dunn2000@gmail.com>
2015-02-10 18:48:01 -0600

    Added initial unity stuff

---
+++ Assets/GM.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+var theSkinLeft : Texture;
+var theSkinRight : Texture;
+function OnGUI () {
+if (!theSkinLeft) {
+			Debug.LogError("Please assign a texture on the inspector");
+			return;
+		}
+	if (GUI.Button(Rect(Screen.width - 682,10,50,50),theSkinLeft))
+			Debug.Log("Clicked the left");	
+if (!theSkinRight) {
+			Debug.LogError("Please assign a texture on the inspector");
+			return;
+		}
+	if (GUI.Button(Rect(Screen.width - 50,10,50,50),theSkinRight))
+			Debug.Log("Clicked the right");	
+}
+
+
