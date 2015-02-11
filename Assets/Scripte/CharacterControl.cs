c57bb6e11371a29cd0334a0aa48a09f1a99ca328
Gavin Dunn <gavin.dunn2000@gmail.com>
2015-02-10 20:37:16 -0600

    it

---
+++ Assets/Scripte/CharacterControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+public class CSharp : MonoBehaviour {
+	public float upspeed = 75.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool jetpackActive = Input.GetButton("Fire1");
+		
+		if (jetpackActive)
+		{
+			rigidbody2D.AddForce(new Vector2(0, upspeed));
+		}
+	}
+	
+}
