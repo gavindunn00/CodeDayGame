1da689dbfd5ec4c763e5372b149a1f793ddc20c5
Gavin Dunn <gavin.dunn2000@gmail.com>
2015-02-10 20:36:49 -0600

    ADDED WORKING GRAVITY

--- Assets/Scripte/GM.js
+++ Assets/Scripte/GM.js
@@ -6,7 +6,7 @@ if (!theSkinLeft) {
 			Debug.LogError("Please assign a texture on the inspector");
 			return;
 		}
-	if (GUI.Button(Rect(Screen.width - 682,10,50,50),theSkinLeft))
+	if (GUI.Button(Rect(0,10,50,50),theSkinLeft))
 			Debug.Log("Clicked the left");	
 if (!theSkinRight) {
 			Debug.LogError("Please assign a texture on the inspector");
